43d93febc6d784e2df488e4af2df7f2f72055f4c
Firtnn <HectorBrun@outlook.fr>
2023-09-06 11:07:55 +0200

    TPMP

---
+++ OvniTeam34/Assets/Scripts/ScaleBox.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScaleBox : MonoBehaviour
+{
+    private Image _borderImage;
+    private float _width;
+    private float _height;
+
+
+    public void InitializeBox()
+    {
+        
+    }
+}
