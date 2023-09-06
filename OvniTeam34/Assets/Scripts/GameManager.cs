6cc7faa29c7c6b00636eb51d8d318eb10c2c9cd7
djika17 <jules.krabal@gmail.com>
2023-09-06 11:05:25 +0200

    advancedGameManager

--- OvniTeam34/Assets/Scripts/GameManager.cs
+++ OvniTeam34/Assets/Scripts/GameManager.cs
@@ -23,6 +23,7 @@ public class GameManager : MonoBehaviour
 
     private float _score;
     private int _numberOfSymbols = 24;
+    [SerializeField] private int _numberOfSentences = 10;
 
     [SerializeField] private List<string> _symbols = new List<string>();
 
@@ -35,6 +36,11 @@ public class GameManager : MonoBehaviour
         for (int i = 0; i < _numberOfSymbols; i++)
         {
             _symbols.Add("a" + i.ToString());
+        }
+
+        for (int i = 0; i < _numberOfSentences; i++)
+        { 
+
         }
     }
 }
--- OvniTeam34/Assets/Scripts/SentenceManager.cs
+++ OvniTeam34/Assets/Scripts/SentenceManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SentenceManager : MonoBehaviour
 {
@@ -8,13 +9,24 @@ public class SentenceManager : MonoBehaviour
 
     private int _numberOfSymbolsInSentence;
 
-    private float _timeScale = 1;
-    private int maxTime = 1;
-    [SerializeField] private float _time = 0f;
-    
+    [SerializeField] private float _timeScale;
+
+    [SerializeField] private float _presidentTalkTime = 0f;
+    [SerializeField] private float _translationTime = 0f;
+
+    private float _presidentMaxTime = 1f;
+    private float _translationMaxTime = 1f;
+
+    private bool _isPresidentTalking = false;
+    private bool _isTranslating = false;
 
 
-    private List<int> _symbolsInSentence = new List<int>();
+    [SerializeField] private UnityEvent _onStartPresidentDialogue;
+    [SerializeField] private UnityEvent _onStartTranslation;
+    [SerializeField] private UnityEvent _onStopPresidentDialogue;
+    [SerializeField] private UnityEvent _onStopTranslation;
+
+    //private List<int> _symbolsInSentence = new List<int>();
 
     // Start is called before the first frame update
     private void Awake()
@@ -23,15 +35,61 @@ public class SentenceManager : MonoBehaviour
     }
     void Start()
     {
-        for (int i = 0; i<_numberOfSymbolsInSentence; i++)
-        {
-            
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        _time += Time.deltaTime*_timeScale;
+        if(_presidentTalkTime <= _presidentMaxTime && _isPresidentTalking)
+        {
+            _presidentTalkTime += Time.deltaTime * _timeScale;
+        }
+        if (_translationTime <= _translationMaxTime && _isTranslating)
+        {
+            _translationTime += Time.deltaTime * _timeScale;
+        }
+    }
+
+    public void StartPresidentDialogue()
+    {
+        _onStartPresidentDialogue.Invoke();
+        _isPresidentTalking = true;
+    }
+
+    public void StopPresidentDialogue()
+    {
+        _onStopPresidentDialogue.Invoke();
+        _isPresidentTalking = false;
+    }
+    public void StartTranslation()
+    {
+        _onStartTranslation.Invoke();
+        _isTranslating = true;
+    }
+
+    public void StopTranslation()
+    {
+        _onStopTranslation.Invoke();
+        _isTranslating = false;
+    }
+
+    public void TranslatorDialogue()
+    {
+        StartCoroutine(PrintingTranslationDelay());
+    }
+
+    public void GenerateSentence(int sentenceSize)
+    {
+        List<int> sentence = new List<int>();
+        for (int i = 0; i < sentenceSize; i++)
+        {
+            //int symbol =
+            //sentence.Add(symbol);
+        }
+    }
+
+    IEnumerator PrintingTranslationDelay()
+    {
+        yield return new WaitForSeconds(2f);
     }
 }
