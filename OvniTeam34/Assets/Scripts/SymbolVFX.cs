46a829461a4df4613d251e0af63f6416ef440be9
Firtnn <HectorBrun@outlook.fr>
2023-09-06 17:22:40 +0200

    Symbole WIP

--- OvniTeam34/Assets/Scripts/SymbolVFX.cs
+++ OvniTeam34/Assets/Scripts/SymbolVFX.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SymbolVFX : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class SymbolVFX : MonoBehaviour
     [SerializeField] private float _lerpTime;
     [SerializeField] float _startValue = 0; 
     [SerializeField] float _endValue = 10;
+    [SerializeField] private Image _symboleImage;
     
 
     public void FadeInSymbol()
@@ -20,6 +22,12 @@ public class SymbolVFX : MonoBehaviour
 
     private IEnumerator LerpThis()
     {
-        yield return null;
+        float timeElapsed = 0;
+        while (timeElapsed < _lerpTime)
+        {
+            
+            yield return null;
+        }
+        
     }
 }
