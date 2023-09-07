08e72054be412f431a6881d1c851901ab1f63ad1
djika17 <jules.krabal@gmail.com>
2023-09-07 12:01:10 +0200

    IntroMenu

--- OvniTeam34/Assets/Scripts/GameManager.cs
+++ OvniTeam34/Assets/Scripts/GameManager.cs
@@ -4,20 +4,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
+using static UnityEngine.InputManagerEntry;
 
 public class GameManager : MonoBehaviour
 {
     public SentenceManager sentenceManager;
     private float _score;
-    [SerializeField] private int _numberOfSentences = 2;
+    [SerializeField] private int _numberOfSentences;
 
+    private bool _isPlaying = false;
+
+    private GameObject _menuIntroUI;
+    private GameObject _clock;
+    private GameObject _presBox;
 
     private List<int> _symbolsInBubble = new List<int>();
 
     private float _voiceSpeed;
 
+    public void StartGame()
+    {
+        _menuIntroUI.SetActive(false);
+        _presBox.SetActive(true);
+        _clock.SetActive(true);
+        sentenceManager.GlobalLoop(_numberOfSentences);
+        _isPlaying = true;
+    }
+
     private void Start()
     {
-        sentenceManager.GlobalLoop(_numberOfSentences);
+        _menuIntroUI = GameObject.Find("IntroMenu");
+        _clock = GameObject.Find("Clock");
+        _presBox = GameObject.Find("PresBox");
+        _presBox.SetActive(false);
+        _clock.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!_isPlaying && Input.anyKey)
+        {
+            StartGame(); 
+        }
     }
 }
